581fab21043a8cf4307921d4113d0fd40b0f1eab
ashx3s <ashx3s@tuta.io>
2025-08-15 16:54:37 -0600

    css and tailwind config changes for newer version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-import { Layout } from "nextra-theme-docs";
-import { getPageMap } from "nextra-theme-docs";
 import type { Metadata } from "next";
 import "nextra-theme-docs/style.css";
 // import "./globals.css";
-//import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-//const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Web Dev 2",
   description: "Sait's software development diploma Web Dev 2 course",
@@ -17,9 +15,7 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" dir="ltr">
-      <body>
-        <Layout pageMap={await getPageMap()}>{children}</Layout>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
