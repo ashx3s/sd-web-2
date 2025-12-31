4f7b3166923dccfc2456cbb89c3ef6697ac5dc00
Ashlyn Knox <ashx3s@tuta.io>
2025-12-31 15:59:02 -0700

    updates and syntax fixes
    version and import syntax for nextra

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
 import nextra from "nextra";
+
 const withNextra = nextra({});
-export default withNextra();
+
+export default withNextra({
+  gitTimestamp: false,
+  turbopack: {
+    resolveAlias: {
+      "next-mdx-import-source-file": "./mdx-components.js",
+    },
+  },
+});
